58522760eea80a2430a0fb5e30361453be64cce3
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2020-06-15 11:40:05 +0930

    Update VBA_Notes.vb

--- VBA_Notes.vb
+++ VBA_Notes.vb
@@ -1,34 +1,3 @@
-function RangePoints(ary as variant) as string
-
-RangePoints = ""
-
-For i = 1 to ubound(index)
-
-	RangePoints = RangePoints & "{ label: '" & ary(i,1) & "' y: " & ary(i,2) & ", " & ary(i,2) & ", " & ary(i,2) & ", " & ary(i,2) & ", " & ary(i,2) & " }," & vbnewline
-
-next i
-
-end function
-
-
-
-Function TwoArraysToOneDataSet(ary1 as variant, ary2 as varient) as variant
-
-dim ary as variant
-redim ary(1 to ubound(ary1), 1 to 2)
-
-for i = 1 to ubound(ary1)
-
-ary(i,1) = ary1(i,1)
-ary(i,2) = ary2(i,1)
-
-next i
-
-TwoArraysToOneDataSet = ary
-
-end function
-
-
 Function chart( _
 		  index as string, _
 		  values as string, _
