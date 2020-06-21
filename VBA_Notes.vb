8c2df1932ffee906c27c806b80744cbf9e1064f5
Kristopher Takken <63102467+kjtakke@users.noreply.github.com>
2020-06-21 21:09:07 +0930

    Update VBA_Notes.vb

--- VBA_Notes.vb
+++ VBA_Notes.vb
@@ -1,66 +1,146 @@
-Const WDdata as variant
-Const MD as String = ""
-Const PP as String = ""
-Const PD as String = ""
-Const RR as String = ""
-Const RD as String = ""
-Const CC as String = ""
-Const CD as String = ""
+'Public Arrays
+Public WDdata as variant            'Website Data Aarray
+Public ColumnText as Variant        'Column Text Field
+Public JavaScriptText as Variant    'JavaScript Field
+
+'Deliminators
+Const MD as String = "C:2s3ZpnC8A,S{T/H)SWZ'24\mmuv3Egb%M/QDA86AUer`zn=Z'u@8;tTry{gqYa5VK`.(y9LvR~&PTs\=RQW2<}A@s:#Lr>V(W2;-s4~$Wbq9~NT'},Q.bm*Rj'7Nve" 'Metadata
+Const PP as String = "^cv8CR(U<3wbvh2>*ee.bK'6b:ZQqwj@s#?EQLhU:U>4Q:^[pALeg,/a+/]R$ZuG48_rTuC9)kQyKUZUe:#jv_.DK$3fm}g%*]~/,`A&$V;5;[yAz$BPw}TV`yXqB~G%" 'Page
+Const PD as String = "C3`:j~52,`/Bt:b:y~y[^PRtznp8^XE-vSA:93=#LjLR>M~8%%$jB<x<G;5)*cB4sPFV9#}/Rd5E8^)<@NazNjEX8S~ND&Qk/Mt_n&3?Y5Dbxx[GNG#En,GZ&k-3RhD:" 'Page Data 
+Const RR as String = "2@Nf<S>GH3VQEvZY+GSw:*-@(?%DV_h{#6AZp'6{DL`~w.cM<U$;8e'BqhyCpSZ2WQ'%}]N+6]xf`pT@,_b@a-g2[]*Hh!8}U4ngnYFVWgyV$y?::]D&bBw[fWD}Y~GF" 'Row
+Const RD as String = "ED!>g9b)u&;4>?~>f'#3_x=S=Pjmstm:CZ/r'tY4A[fMk5~P%C>*77*)<u^9'sUXGWhKpZ9RtJ{%{zrABU4~Mrmh4MuS,,pGsSDEv4)[~F$M6PbCUEdA9gGgP'tbQzPn" 'Row Data
+Const CC as String = "`YE%%8y8zGX_d7<y*FDSG3!h.\KF2qQf%A#z8[v\@ML~bU#ehM<U+aV3t,7YdwgU>ydR_E^V>4xzGXfP;c3j.a45QFJwRxv/pD:=5QEK~4@Q7m5]KkaD5;!#q_T6t$&'" 'Column
+Const CD as String = "7,)zbZ,>-_*/*6yeLSX&~@@y,k6$HksXyX~ex}#g&(AL\yDY(kcn9!`xQg9$[GyVq;(a/vC$4T=^+jB?yL3M8m'u]76F)v/XaEV'#>K?f5g=5]7>^h!y4:%c{_SW*fky" 'Column Data
+Const TD as String = "auY5xUf454Ajc6~~}.CS.<DZU7bB?Ee.+;YZ5$J?N9!68.~fgrquYj]{A,5Rfe$(;=caBe*\g!$%b4REtwkn6w]]cT>N[T([VE_J?%}$DNak`w)@:58zse[<4M#d.Zp6" 'Text Column Data
+Const JD as String = "Hn:w_N<mSpmm,w~_DrC,~}:6$yneD:9+KhA>,nr3X+w-:jVQYCpND=]4?-,g[pA)wcN''zffZW(U=?&uXGj&~V%8N^5ryBN`@+MsY!<;x`;r6dd#y*'5@:x2{u_w}LH]" 'JavaScript Data
+
+'Array Dimentions
+Const AD1 as Integer = 100  'WDdata Dimention 1
+Const AD2 as Integer = 100  'WDdata Dimention 2
+Const AD3 as Integer = 30   'WDdata Dimention 3
+Const AD4 as Integer = 300  'WDdata Dimention 4
+
+'Data Fields
+Const DM as integer = 300   'Metadata Fields
+Const DP as integer = 300   'Page Fields
+Const DR as integer = 300   'Row Fields
+Const DC as integer = 300   'Column Fields
 Public XLWD as String
 
-Sub InitiateDataArray
-redim WDdata(0 to 100, 0 to 100, 0 to 30, 0 to 300)
+Sub ConcatinateWDDataArray
+    'P = Page    R = Row      C = Column
 
-'Metadata    0.0.0.0-##
-'Page data   P.0.0.0-##   0 = 0(exclude) or 1(include)
-'Row data    P.R.0.0-##   0 = 0(exclude) or 1(include)
-'Column data P.R.C.0-300  0 = 0(exclude) or 1(include)
+    'Metadata    0.0.0.0-##
+    'Page data   P.0.0.0-##   0 = 0(exclude) or 1(include)
+    'Row data    P.R.0.0-##   0 = 0(exclude) or 1(include)
+    'Column data P.R.C.0-###  0 = 0(exclude) or 1(include)
 
-Call GetWebPageData
+    Dim MTD as varient
+    Dim AllData as String
+    'WDdata(0 to 100, 0 to 100, 0 to 30, 0 to 300)
 
-Dim MTD as varient
-Dim PGS,  RWS,  CLS as varient
-Dim PGSD, RWSD, CLSD as varient
+    AllData = ""
 
-'Metadata
-MTD = split(XLWD, "MM")
-for M = 1 to ubound(MTD)-1
-	WDdata(0,0,0,M-1) = MTD(M,1)
-Next M
-
-'Pages
-PGS = split(XLWD, "PP")
-for P = 2 to ubound(Pages)
+    'Metadata
+    For M=0 to DM
+        AllData = AllData & WDData(0,0,0,M) & MM
+    Next M
+    AllData = AllData & PP
+    'Pages
+    For P = 0 to 100
 
         'Page Data
-	PGSD = split(XLWD, "PD")
-	for PD = 1 to ubound(PGSD)
-		WDdata(P-1,0,0,PD-1) = PGSD(PD,1)
-	next PD
+        For PA = 0 to DP
+            AllData = AllData & WDData(P,0,0,PA) & PD
+        Next PA
         
         'Rows
-	RWS = split(PGS(P,1))
-	for R = 2 to ubound(Rows, "RR")
+        For R = 0 to 100 
 
-                'Row Data
-		RWSD = split(RWS(RD,1))
-		for RD = 1 to ubound(RWSD)
-			WDdata(P-1,R-1,0,RD-1) = RWSD(RD,1)
-		next RD
+            'Row Data
+            For RA = 0 to DR
+                AllData = AllData & WDData(P,R,0,RA) & RD
+            Next RA
 
-                'Columns
-		CLS = split(RWS(R,1))
-		for C = 2 to ubound(CLS, "CC")
+            'Columns
+            For C = 1 to 30
+                'Column Data
+                For CA = 0 to DC 
+                    AllData = AllData & WDData(P,R,C,CA) & CD
+                Next CA
 
-                        'Column Data
-			CLSD = split(CLS(CD,1))
-			for CD = 1 to ubound(CLSD)
-				WDdata(P-1,R-1,C-1,CD-1) = CLSD(CD,1)
-			next CD
-                next C
-	next R
-next P
+                'Concatinate Columns
+                AllData = AllData & CC
+            Next C
 
+            'Concatinate Rows
+            AllData = AllData & RR
+        Next R
+
+        'Concatinate Pages
+        AllData = AllData & PP
+    Next P
+
+    'Write to XLWD File
+    XLWD = AllData
+    Call WriteXLWD(XLWD)
+End Sub
+
+
+Sub SplitWDDataArray
+    redim WDdata(0 to AD1, 0 to AD2, 0 to AD3, 0 to AD4)
+
+    'P = Page    R = Row      C = Column
+
+    'Metadata    0.0.0.0-##
+    'Page data   P.0.0.0-##   0 = 0(exclude) or 1(include)
+    'Row data    P.R.0.0-##   0 = 0(exclude) or 1(include)
+    'Column data P.R.C.0-###  0 = 0(exclude) or 1(include)
+
+    Call GetWebPageData
+
+    Dim MTD as varient
+    Dim PGS,  RWS,  CLS as varient
+    Dim PGSD, RWSD, CLSD as varient
+
+    'Metadata
+    MTD = split(XLWD, MM)
+    for M = 1 to ubound(MTD)-1
+        WDdata(0,0,0,M-1) = MTD(M,1)
+    Next M
+
+    'Pages
+    PGS = split(XLWD, PP)
+    for P = 2 to ubound(PGS)
+
+        'Page Data
+        PGSD = split(PGS(P,1), PD)
+        for PA = 1 to ubound(PGSD) - 1
+            WDdata(P-1,0,0,PA-1) = PGSD(PA,1)
+        next PA
+            
+        'Rows
+        RWS = split(PGS(P,1), RR)
+        for R = 2 to ubound(RWS)
+
+            'Row Data
+            RWSD = split(RWS(R,1),RD)
+            for RA = 1 to ubound(RWSD) - 1
+                WDdata(P-1,R-1,0,RA-1) = RWSD(RA,1)
+            next RA
+
+            'Columns
+            CLS = split(RWS(R,1),CC)
+            for C = 2 to ubound(CLS)
+
+                'Column Data
+                CLSD = split(CLS(C,1),CD)
+                for CA = 1 to ubound(CLSD) - 1
+                    WDdata(P-1,R-1,C-1,CA-1) = CLSD(CA,1)
+                next CA
+            next C
+        next R
+    next P
 End Sub
 
 
